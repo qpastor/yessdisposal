df470178af14eabd6a58ce181f6f1ee10c661bc2
Qin Lark Pastor <qinpastor26@gmail.com>
2026-06-19 03:29:06 +0800

    Fix API Base URL

--- client/src/api.js
+++ client/src/api.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 //   ? 'https://yess-disposal-api.onrender.com'
 //   : 'http://localhost:5001';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
+const API_BASE_URL = import.meta.env.VITE_API_URL //|| 'http://localhost:5001';
 
 const instance = axios.create({
   // If the environment variable fails, it falls back to localhost
