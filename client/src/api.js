eab0ab7941541bcb765f5a6adb7fd5289481ffba
Qin Lark Pastor <qinpastor26@gmail.com>
2026-06-23 03:09:36 +0800

    Updated API from local to PROD

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 // Detect the API URL based on the environment
-// const API_BASE_URL = import.meta.env.MODE === 'production'
-//   ? 'https://yess-disposal-api.onrender.com'
-//   : 'http://localhost:5001';
+const API_BASE_URL = import.meta.env.MODE === 'production'
+  ? 'https://yess-disposal-api.onrender.com'
+  : 'http://localhost:5001';
 
-const API_BASE_URL = 'http://localhost:5001';
+// const API_BASE_URL = 'http://localhost:5001';
 
 const instance = axios.create({
   // If the environment variable fails, it falls back to localhost
